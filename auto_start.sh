6d4ec57a1d7ce85f4a3c6fe2298bc21b2033c32c
Luis Fernando Murara <murara@atontech.net>
2017-04-25 16:17:45 -0300

    Add instrunctions to proceed with host

--- auto_start.sh
+++ auto_start.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# First you need to setup the host:
+#  $ xe pool-list
+#  $ xe pool-param-set uuid=XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX other-config:auto_poweron=true
+
 for VMUUID in $(xe vm-list | grep uuid | awk '{print $5}')
   do
   xe vm-param-set uuid=$VMUUID other-config:auto_poweron=true
